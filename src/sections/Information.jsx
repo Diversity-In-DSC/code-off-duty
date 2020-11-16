11c9c6ca823444ecdd3933a86e7c440e9877d488
VarunIrani <varunirani0505@gmail.com>
2020-11-16 15:50:32 +0530

    Countdown done 🕙

--- src/sections/Information.jsx
+++ src/sections/Information.jsx
@@ -30,7 +30,45 @@ const eligibilityAndRules = {
 
 // What is Code Off Duty?
 class Information extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  getCountdown() {
+    let days = new Date(2020, 11, 11, 10, 0, 0);
+    days = days / 1E3;
+    let hours = Date.now();
+    hours = hours / 1E3;
+    let seconds = days - hours;
+    days = Math.floor(seconds / 86400);
+    hours = Math.floor((seconds - 86400 * days) / 3600);
+    let minutes = Math.floor((seconds - 86400 * days - 3600 * hours) / 60);
+    seconds = Math.floor(seconds - 86400 * days - 3600 * hours - 60 * minutes);
+    "10" > hours && (hours = "0" + hours);
+    "10" > minutes && (minutes = "0" + minutes);
+    "10" > seconds && (seconds = "0" + seconds);
+    return {days, hours, minutes, seconds}
+  }
+
+  componentDidMount() {
+    setInterval(function () {
+      this.setState(this.getCountdown())
+    }.bind(this))
+  }
+
   render() {
+    const countdownData = [
+      {label: "Days", color: "#EA4435", value: this.state.days},
+      {label: "Hours", color: "#34A852", value: this.state.hours},
+      {label: "Minutes", color: "#4385F4", value: this.state.minutes},
+      {label: "Seconds", color: "#FBBD04", value: this.state.seconds},
+    ]
     return (
       <Container className="py-5">
         <Row className="my-5 justify-content-center">
@@ -69,14 +107,29 @@ class Information extends Component {
             </h5>
           </Col>
         </Row>
-
+        {/*Countdown*/}
+        <Row className="h2 font-weight-bold mt-5 justify-content-center text-center mx-4" style={{color: "#ffe05d"}}>COUNTDOWN TILL
+          HACKATHON</Row>
+        <Row className="text-white justify-content-center mt-5">
+          {countdownData.map((data, index) => <Col xl={2} md={5} key={index}>
+            <Row className="justify-content-center h1"><p
+              style={{color: data.color, fontWeight: "bold"}}>{data.value}</p></Row>
+            <Row className="justify-content-center h4 mb-5">{data.label}</Row>
+          </Col>)}
+        </Row>
+        {/*Rules and Eligibility*/}
         <Row className="justify-content-center justify-content-around mt-5">
           <Col className="mb-5 mb-xl-0"
-               style={{border: "2px solid #ffe05d", borderRadius: 12, boxShadow: "10px -10px rgba(255, 224, 93, 0.5)", backgroundColor: "#111"}} xs={11} xl={5}>
+               style={{
+                 border: "2px solid #ffe05d",
+                 borderRadius: 12,
+                 boxShadow: "10px -10px rgba(255, 224, 93, 0.5)",
+                 backgroundColor: "#111"
+               }} xs={11} xl={5}>
             <Row className="text-white justify-content-center my-3">
               <h3 style={{fontFamily: "Valorant"}}>Rules</h3>
             </Row>
-            {eligibilityAndRules.rules.map(rule => <Row className="mb-3 px-4">
+            {eligibilityAndRules.rules.map((rule, index) => <Row className="mb-3 px-4" key={index}>
               <h6
                 style={{
                   fontWeight: "bold",
@@ -85,11 +138,16 @@ class Information extends Component {
             </Row>)}
           </Col>
           <Col className="my-auto"
-               style={{border: "2px solid #ffe05d", borderRadius: 12, boxShadow: `10px -10px rgba(255, 224, 93, 0.5)`, backgroundColor: "#111"}} xs={11} xl={5}>
+               style={{
+                 border: "2px solid #ffe05d",
+                 borderRadius: 12,
+                 boxShadow: `10px -10px rgba(255, 224, 93, 0.5)`,
+                 backgroundColor: "#111"
+               }} xs={11} xl={5}>
             <Row className="text-white justify-content-center my-3">
               <h3 style={{fontFamily: "Valorant"}}>Eligibility</h3>
             </Row>
-            {eligibilityAndRules.eligibility.map(rule => <Row className="mb-3 px-4">
+            {eligibilityAndRules.eligibility.map((rule, index) => <Row className="mb-3 px-4" key={index}>
               <h6 className="text-white" style={{fontWeight: "bold"}}>{rule.rule}</h6>
             </Row>)}
           </Col>
