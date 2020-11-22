72677f8a0e29bee9bb9987baed090ceb4a6d0fc0
kulkarniprem04 <kulkarniprem04@gmail.com>
2020-11-22 09:58:49 +0530

    pull commit

--- src/sections/FAQ.js
+++ src/sections/FAQ.js
@@ -1,111 +1,143 @@
-import React, {Component} from 'react';
-import {Accordion, Card, Col, Container, Row} from "react-bootstrap";
+import React, { Component } from "react";
+import { Accordion, Card, Col, Container, Row } from "react-bootstrap";
 import FAQData from "../data/FAQData";
-import {ReactComponent as PlayBtn} from "../assets/img/playbtn.svg";
-import {ReactComponent as RePlayBtn} from "../assets/img/replay.svg";
-import "../css/FAQ.css"
-import {WindupChildren} from "windups";
+import { ReactComponent as PlayBtn } from "../assets/img/playbtn.svg";
+import { ReactComponent as RePlayBtn } from "../assets/img/replay.svg";
+import "../css/FAQ.css";
+import { WindupChildren } from "windups";
 
 class FAQ extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      FAQData: FAQData
-    }
+      FAQData: FAQData,
+    };
   }
 
   render() {
     return (
-      <Container fluid className="fixed-top px-5" style={this.props.style} id="faq-section">
+      <Container
+        fluid
+        className="fixed-top px-5"
+        style={this.props.style}
+        id="faq-section"
+      >
         <Row>
           <Col>
             <Row className="mx-5 align-items-center mt-4">
-              <Col xl={1} className="my-auto ml-5" onClick={() => {
-                this.props.goBack("faq")
-              }}>
-                <svg width="2em" height="2em" viewBox="0 0 13 10"
-                     style={{
-                       stroke: "#fff",
-                       fill: "none",
-                       strokeWidth: 2,
-                       transform: "rotateY(180deg)",
-                       cursor: "pointer"
-                     }}>
-                  <path d="M1,5 L11,5"/>
-                  <polyline points="8 1 12 5 8 9"/>
+              <Col
+                xl={1}
+                className="my-auto ml-5"
+                onClick={() => {
+                  this.props.goBack("faq");
+                }}
+              >
+                <svg
+                  width="2em"
+                  height="2em"
+                  viewBox="0 0 13 10"
+                  style={{
+                    stroke: "#fff",
+                    fill: "none",
+                    strokeWidth: 2,
+                    transform: "rotateY(180deg)",
+                    cursor: "pointer",
+                  }}
+                >
+                  <path d="M1,5 L11,5" />
+                  <polyline points="8 1 12 5 8 9" />
                 </svg>
               </Col>
               <Col xl={2} className="p-0 ml-5">
-                <span style={{fontFamily: "Blockletter", fontSize: 42}}>FAQs</span>
+                <span style={{ fontFamily: "Blockletter", fontSize: 42 }}>
+                  FAQs
+                </span>
               </Col>
             </Row>
-
           </Col>
-          <Col className="mt-5" style={{
-            backgroundColor: "rgba(0, 0, 0, 0)",
-            paddingTop: "5%",
-          }}>
+          <Col
+            className="mt-5"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0)",
+              paddingTop: "5%",
+            }}
+          >
             <Accordion as={Col} className="mt-2">
               {this.state.FAQData.map((data, number) => (
-                <Card style={{
-                  backgroundColor: "rgba(0, 0, 0, 0)",
-                  borderColor: "rgba(0, 200, 70, 0.5)"
-                }}>
-                  <Accordion.Toggle as={Card.Header} eventKey={number.toString()}
-                                    onClick={() => {
-                                      let fqaData = this.state.FAQData;
-                                      fqaData.forEach((d, index) => {
-                                        d.played = index === number ? !d.played : false;
-                                      })
-                                      this.setState({FAQData: fqaData});
-                                    }} style={{
-                    borderBottomColor: "rgba(0, 200, 70, 0.5)",
-                    borderBottomStyle: "dashed",
-                  }}>
+                <Card
+                  style={{
+                    backgroundColor: "rgba(0, 0, 0, 0)",
+                    borderColor: "rgba(0, 200, 70, 0.5)",
+                  }}
+                >
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    eventKey={number.toString()}
+                    onClick={() => {
+                      let fqaData = this.state.FAQData;
+                      fqaData.forEach((d, index) => {
+                        d.played = index === number ? !d.played : false;
+                      });
+                      this.setState({ FAQData: fqaData });
+                    }}
+                    style={{
+                      borderBottomColor: "rgba(0, 200, 70, 0.5)",
+                      borderBottomStyle: "dashed",
+                    }}
+                  >
                     <Row>
                       <Col sm={11}>
                         <h5>
-                          <span style={{fontFamily: "Blockletter"}}>
-                            Question>
+                          <span style={{ fontFamily: "Blockletter" }}>
+                            Question
                           </span>
-                          <snap style={{fontFamily: "Poppins", fontSize: 18}}>
+                          <snap style={{ fontFamily: "Poppins", fontSize: 18 }}>
                             {" " + data.question}
                           </snap>
                         </h5>
                       </Col>
                       <Col>
-                        {
-                          !data.played ?
-                            <PlayBtn style={{width: 20, height: 20}}/> :
-                            <RePlayBtn style={{
+                        {!data.played ? (
+                          <PlayBtn style={{ width: 20, height: 20 }} />
+                        ) : (
+                          <RePlayBtn
+                            style={{
                               width: 20,
                               height: 20,
                               animationName: "rotate-object",
                               animationDuration: "1s",
                               animationIterationCount: 1,
                               transitionTimingFunction: "ease-in",
-                            }}/>
-                        }
+                            }}
+                          />
+                        )}
                       </Col>
                     </Row>
                   </Accordion.Toggle>
                   <Accordion.Collapse eventKey={number.toString()}>
                     <Card.Body>
                       <h5>
-                        <span style={{fontFamily: "Blockletter"}}>
-                          Answer>
+                        <span style={{ fontFamily: "Blockletter" }}>
+                          Answer
                         </span>
-                        <snap style={{
-                          fontFamily: "Poppins",
-                          fontSize: 18
-                        }}>
-                          {data.played && <WindupChildren speed={500}>
-                            {" " + data.answer}
-                          </WindupChildren>}
+                        <snap
+                          style={{
+                            fontFamily: "Poppins",
+                            fontSize: 18,
+                          }}
+                        >
+                          {data.played && (
+                            <WindupChildren speed={500}>
+                              {" " + data.answer}
+                            </WindupChildren>
+                          )}
                         </snap>
-                        <snap style={{
-                          animation: "blink 0.9s infinite",
-                        }}>|
+                        <snap
+                          style={{
+                            animation: "blink 0.9s infinite",
+                          }}
+                        >
+                          |
                         </snap>
                       </h5>
                     </Card.Body>
