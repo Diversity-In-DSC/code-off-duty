f7fa98b7cc5d2f1494acaf41db77127e981e71f4
VarunIrani <varunirani0505@gmail.com>
2020-11-22 15:30:57 +0530

    Font size for What is COD

--- src/sections/Information.js
+++ src/sections/Information.js
@@ -26,6 +26,7 @@ const eligibilityAndRules = {
     }
   ],
 }
+
 class Information extends Component {
   render() {
     return (
@@ -51,19 +52,23 @@ class Information extends Component {
           </Col>
         </Row>
         <Row>
-         <Card className="bg-transparent text-white" style ={{border:"none"}} >
-         <Card.Body><p><b>CODE-OFF-DUTY</b> is an event of <b>DSC WOW</b>, where innovaters get an opportunity to merge their creative ideas 
-           with their coding skills to build something exemplary. It's a place where your ideas will get acknowledeged.</p>
-           Expect more than 36 hrs of inspiring panel discussion,working and collaborating on futuristic and empowering tech products, networking 
-           opportunities with budding developers and lots of fun!
-         </Card.Body>
-         </Card>
+          <Card className="bg-transparent text-white p-4" style={{border: "none"}}>
+            <Card.Body><p style={{fontSize: 20}}><b>CODE-OFF-DUTY</b> is an event of <b>DSC WOW</b>, where innovators
+              get an opportunity to merge their creative ideas
+              with their coding skills to build something exemplary. It's a place where your ideas will get
+              acknowledged.</p>
+              <p style={{fontSize: 20}}>Expect more than 36 hrs of inspiring panel discussion,working and collaborating
+                on futuristic and empowering tech products, networking
+                opportunities with budding developers and lots of fun!</p>
+            </Card.Body>
+          </Card>
         </Row>
         <Row className="justify-content-center justify-content-around mt-5">
           <Col className="mb-5 mb-xl-0"
                style={{
-                borderStyle:"dashed",
-                 borderColor:"rgb(0,255,70)",
+                 borderWidth: "5px",
+                 borderStyle: "dashed",
+                 borderColor: "rgb(0,255,70)",
                  borderRadius: 10,
                  backgroundColor: "none",
                }} xs={10} xl={5}>
@@ -71,29 +76,29 @@ class Information extends Component {
               <h3 style={{fontFamily: "Blockletter"}}>Rules</h3>
             </Row>
             {eligibilityAndRules.rules.map((rule, index) => <Row className="mb-3 px-4" key={index}>
-              <h6
+              <h5
                 style={{
-                
+
                   color: rule.type ? "rgb(255, 255, 255)" : "rgb(255,255,255)"
-                }}>{rule.rule}</h6>
+                }}>{rule.rule}</h5>
             </Row>)}
           </Col>
           <Col className="mb-5 mb-xl-0 "
                style={{
+                 borderWidth: "5px",
                  borderRadius: 10,
-                 borderStyle:"dashed",
-                 borderColor:"rgb(0,255,70)"
-
+                 borderStyle: "dashed",
+                 borderColor: "rgb(0,255,70)"
                }} xs={10} xl={5}>
             <Row className="bg-transparent text-white justify-content-left p-3">
               <h3 style={{fontFamily: "Blockletter"}}>Eligibility</h3>
             </Row>
             {eligibilityAndRules.eligibility.map((rule, index) => <Row className="mb-3 px-4" key={index}>
-              <h6 className="text-white">{rule.rule}</h6>
+              <h5 className="text-white">{rule.rule}</h5>
             </Row>)}
           </Col>
         </Row>
-      </Container> 
+      </Container>
     );
   }
 }
