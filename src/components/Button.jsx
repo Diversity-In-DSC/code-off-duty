cbb2e4622fbac16be57af70d029e7dc404837643
atmanad <atmanad0001@gmail.com>
2020-11-23 23:21:33 +0530

    made moible responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,15 +7,14 @@ export const ButtonHover = props => {
     className, onClick, label, isDisabled, style
   } = props;
   return (
-    <button onClick={onClick} disabled={isDisabled} style={style}>
-      <span className={className} style={{borderRadius: 50, border: "2px solid rgb(0, 255, 70)"}}>
-        <span style={{
-          fontFamily: 'Blockletter', fontSize: 24, letterSpacing: 4, ":hover": {}
-        }} id="label"
-              className="bg-transparent">{label}</span>
+    <button onClick={onClick} disabled={isDisabled} style={{ verticalAlign: 'middle' }}>
+      <span className={className} style={{ borderRadius: 50, border: "2px solid rgb(0, 255, 70)" }}>
+        <span style={{ fontFamily: 'Blockletter', letterSpacing: 4, ":hover": {} }}
+          id="label"
+          className="bg-transparent">{label}</span>
         <svg width="1.2em" height="2em" viewBox="0 0 13 10">
-          <path d="M1,5 L11,5"/>
-          <polyline points="8 1 12 5 8 9"/>
+          <path d="M1,5 L11,5" />
+          <polyline points="8 1 12 5 8 9" />
         </svg>
       </span>
     </button>
